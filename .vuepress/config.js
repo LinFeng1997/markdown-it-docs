4f50fec0735f7de0f66546fe728e8ba1c532aa43
许云峰 <xuyunfeng.fe@bytedance.com>
2019-01-18 19:44:56 +0800

    chore:百度统计脚本

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,26 +1,31 @@
 module.exports = {
-    "themeConfig": {
-        "sidebar": ["/", "/api/Core.md", "/api/MarkdownIt.md", "/api/ParserBlock.md", "/api/ParserInline.md", "/api/Renderer.md", "/api/Ruler.md", "/api/Token.md"],
-        "nav": [{
-                "key": "/architecture.md",
-                "label": "架构",
-                "link": "/architecture.md",
-                "text": "架构"
-            },
-            {
-                "key": "/development.md",
-                "label": "开发者建议",
-                "link": "/development.md",
-                "text": "开发者建议"
-            }, {
-                "key": "/security.md",
-                "label": "安全",
-                "link": "/security.md",
-                "text": "安全"
-            }
-        ]
+  "themeConfig": {
+    "sidebar": ["/", "/api/Core.md", "/api/MarkdownIt.md", "/api/ParserBlock.md", "/api/ParserInline.md", "/api/Renderer.md", "/api/Ruler.md", "/api/Token.md"],
+    "nav": [{
+      "key": "/architecture.md",
+      "label": "架构",
+      "link": "/architecture.md",
+      "text": "架构"
     },
-    "less": { "javascriptEnabled": true },
-    "dest": "/Users/xuyunfeng/Desktop/play/markdown-it/.vuepress/markdown-it",
-    "title": "markdown-it"
+      {
+        "key": "/development.md",
+        "label": "开发者建议",
+        "link": "/development.md",
+        "text": "开发者建议"
+      }, {
+        "key": "/security.md",
+        "label": "安全",
+        "link": "/security.md",
+        "text": "安全"
+      }
+    ]
+  },
+  "less": {"javascriptEnabled": true},
+  "dest": "/Users/xuyunfeng/Desktop/play/markdown-it/.vuepress/markdown-it",
+  "title": "markdown-it",
+  head: [
+    ['script', {
+      src: `/baidu.js`
+    }]
+  ]
 }
