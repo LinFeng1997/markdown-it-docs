cc7e97038c461a00db0dc8be5b7796d46ad4b62c
许云峰 <xuyunfeng.fe@bytedance.com>
2019-01-18 19:55:45 +0800

    deploy

---
+++ baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?b08894dca36cc7f672a0eb2a89d319a5";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
