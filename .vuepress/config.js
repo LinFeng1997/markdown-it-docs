4a1ed8b9c9e320ad7829e9e8f63eb4a32191ebc0
lizhihua <275091674@qq.com>
2019-04-10 14:33:16 +0800

    修复统计脚本不生效的问题

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,32 +1,21 @@
-const path = require('path');
+const path = require('path')
 module.exports = {
-  "themeConfig": {
-    "sidebar": ["/", "/api/Core.md", "/api/MarkdownIt.md", "/api/ParserBlock.md", "/api/ParserInline.md", "/api/Renderer.md", "/api/Ruler.md", "/api/Token.md"],
-    "nav": [{
-      "key": "/architecture.md",
-      "label": "架构",
-      "link": "/architecture.md",
-      "text": "架构"
-    },
-      {
-        "key": "/development.md",
-        "label": "开发者建议",
-        "link": "/development.md",
-        "text": "开发者建议"
-      }, {
-        "key": "/security.md",
-        "label": "安全",
-        "link": "/security.md",
-        "text": "安全"
-      }
-    ]
+  port: 3100,
+  title: 'markdown-it 中文文档',
+  description: '',
+  themeConfig: {
+    nav: [
+      {key: '/architecture.md', label: '架构', link: '/architecture.md', text: '架构'},
+      {key: '/development.md', label: '开发者建议', link: '/development.md', text: '开发者建议'}, {
+      key: '/security.md', label: '安全', link: '/security.md', text: '安全'},
+      {text: '印记中文', link: 'https://docschina.org/'},
+    ],
+    sidebar: ['/', '/api/Core.md', '/api/MarkdownIt.md', '/api/ParserBlock.md', '/api/ParserInline.md', '/api/Renderer.md', '/api/Ruler.md', '/api/Token.md'],
   },
-  "less": {"javascriptEnabled": true},
-  "dest": path.resolve(__dirname,"../markdown-it"),
-  "title": "markdown-it",
+  'less': {'javascriptEnabled': true},
   head: [
     ['script', {
-      src: `/baidu.js`
-    }]
-  ]
+      src: `/js/baidu.js`,
+    }],
+  ],
 }
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
-cd markdown-it
+npm run build
+cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/LinFeng1997/markdown-it-docs.git master:cn-gh-pages
-rm -rf markdown-it
+git push -f https://github.com/docschina/markdown-it-docs.git master:cn-gh-pages
+rm -rf ../dist
