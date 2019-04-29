3f5a601b67d4cb915c159a253e1c82eafe582682
许云峰 <xuyunfeng.fe@bytedance.com>
2019-04-29 10:02:13 +0800

    chore：Travis 秘钥

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,21 @@
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+
 npm run build
+
+# Set some git options
+git config --global user.name "Travis CI"
+git config --global user.email "ci@travis-ci.org"
+git remote set-url origin "${SSH_REPO}"
+
+openssl aes-256-cbc -K $encrypted_7562052d3e34_key -iv $encrypted_7562052d3e34_iv -in scripts/deploy_key.enc -out scripts/deploy_key -d
+chmod 600 scripts/deploy_key
+eval `ssh-agent -s`
+ssh-add scripts/deploy_key
+chmod -R 777 node_modules/gh-pages/
+
+# deploy
 cd dist
 git init
 git add -A
